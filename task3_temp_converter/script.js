5db00416146bb9f8148065893618d4db676c2895
Bsamanvitha-007 <samanvithabodire@gmail.com>
2026-03-15 21:15:05 +0530

    Add files via upload

---
+++ task3_temp_converter/script.js
@@ -0,0 +1,27 @@
+function convertTemp(){
+
+let temp = document.getElementById("temp").value;
+
+let unit = document.getElementById("unit").value;
+
+let result;
+
+if(unit === "celsius"){
+
+result = (temp * 9/5) + 32;
+
+document.getElementById("result").innerHTML =
+result.toFixed(2) + " °F";
+
+}
+
+else{
+
+result = (temp - 32) * 5/9;
+
+document.getElementById("result").innerHTML =
+result.toFixed(2) + " °C";
+
+}
+
+}
